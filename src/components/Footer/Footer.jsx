0800c67a5bddb971fb8a2ac697fc9e22584ff2ea
Kishan Srivastava <kishansrivastava.01.ks@gmail.com>
2024-10-24 23:54:07 +0530

    navbar and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,15 +11,21 @@ import {
 const FooterContainer = styled.footer`
   background-color: black;
   color: white;
-  padding: 3rem 1rem;
+  padding: 5rem 10rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const FirstRow = styled.div`
+  /* background-color: red; */
   display: flex;
-  justify-content: space-between;
+  width: 100%;
+  justify-content: space-around;
+  align-items: flex-start;
   flex-wrap: wrap;
   margin-bottom: 2rem;
-
+  /* border: 2px solid white; */
   @media (max-width: 768px) {
     flex-direction: column;
     align-items: center;
@@ -29,6 +35,8 @@ const FirstRow = styled.div`
 const FooterColumn = styled.div`
   flex: 1;
   margin: 0 1.5rem;
+  /* max-width: 25%; */
+  text-align: left;
 
   @media (max-width: 768px) {
     margin: 1rem 0;
@@ -38,7 +46,8 @@ const FooterColumn = styled.div`
 
 const FooterHeading = styled.h3`
   margin-bottom: 1rem;
-  font-family: var(--brand-font-1);
+  /* font-family: var(--brand-font-1); */
+  letter-spacing: 1px;
 `;
 
 const FooterLink = styled.a`
@@ -46,10 +55,13 @@ const FooterLink = styled.a`
   margin-bottom: 0.5rem;
   color: white;
   text-decoration: none;
+  font-weight: 500;
+  letter-spacing: 1px;
 
   &:hover {
     color: var(--color-main);
   }
+  transition: all 0.2s ease-in-out;
 `;
 
 const SocialIcons = styled.div`
@@ -65,12 +77,14 @@ const SocialIcons = styled.div`
 
 const Divider = styled.hr`
   border: none;
-  border-top: 1px solid white;
+  border-top: 0.1px solid white;
   width: 90%;
   margin: 2rem auto;
+  color: #bbb;
 `;
 
 const SecondRow = styled.div`
+  width: 90%;
   display: flex;
   justify-content: space-between;
   align-items: center;
